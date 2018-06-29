24d4ef8cb80ce4fe53f463beeb9af9de7deb1264
leonesquire <leva2004@yandex.ru>
2018-06-29 10:42:38 +0300

    done

--- validation.js
+++ validation.js
@@ -1,4 +1,23 @@
 // проверочный скрипт писать здесь
+// проверочный скрипт писать здесь
+const inputs = document.querySelectorAll('input')
+
 const patterns = {
 	telephone: /^\d{11}$/
+};
+
+function validate(field, regex) {
+	if (regex.test(field.value)) {
+		field.className = 'valid';
+	} else {
+		field.className = 'invalid';
+	}
 }
+
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', (e) => {
+		//console.log(e.target.attributes.name.value);
+		validate(e.target, patterns[e.target.attributes.name.value]);
+	})
+})		
