bc4530376d0b87fb48506c6a587edf4c2156459d
leonesquire <leva2004@yandex.ru>
2018-06-29 10:41:22 +0300

    done

--- validation.js
+++ validation.js
@@ -1,4 +1,25 @@
 // проверочный скрипт писать здесь
+const inputs = document.querySelectorAll('input')
+
 const patterns = {
-	telephone: /^\d{11}$/
+	telephone: /^\d{11}$/,
+	username: /^[a-z\d]{5,12}$/,
+	password: /^[\w@-]{8,20}$/,
+	slug: /^[a-z\d-]{8,20}$/
+};
+
+function validate(field, regex) {
+	if (regex.test(field.value)) {
+		field.className = 'valid';
+	} else {
+		field.className = 'invalid';
+	}
 }
+
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', (e) => {
+		//console.log(e.target.attributes.name.value);
+		validate(e.target, patterns[e.target.attributes.name.value]);
+	})
+})		
